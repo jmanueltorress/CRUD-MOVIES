f3a40710b63088cc10c971945a9e3a4dea1d3fca
Stiven Medina <71569136+mzrtcode@users.noreply.github.com>
2023-04-18 22:21:42 -0500

    Update CrearCategoria.jsx

--- src/CrearCategoria.jsx
+++ src/CrearCategoria.jsx
@@ -45,11 +45,11 @@ const CrearCategoria = () => {
          <h1 className='text-xl font-bold'>Crear nueva categoria</h1>
   <form className='pt-4' onSubmit={submitHandler}>
     <div className="mb-6">
-      <label htmlFor="nombre" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
+      <label htmlFor="nombre" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Nombre</label>
       <input type="text" id="nombre" name="nombre" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nombre categoria" required></input>
     </div>
     <div className="mb-6">
-      <label htmlFor="descripcion" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descripcion</label>
+      <label htmlFor="descripcion" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Descripcion</label>
       <textarea id="descripcion" name="descripcion"  rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Describe la categoria..."></textarea>
     </div>
     <button  type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Registrar</button>
