c142345704df7a991e27c248160f8c81516c8ec9
Stiven Medina <71569136+mzrtcode@users.noreply.github.com>
2023-04-18 22:22:45 -0500

    Update CrearResena.jsx

--- src/CrearResena.jsx
+++ src/CrearResena.jsx
@@ -70,7 +70,7 @@ const CrearResena = () => {
             <h1 className='text-xl font-bold'>Crear reseña</h1>
             <form className='pt-4' onSubmit={submitHandler}>
                 <div className="mb-6">
-                    <label htmlFor="id_usuario" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Usuario</label>
+                    <label htmlFor="id_usuario" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Usuario</label>
 
                     <select id="id_usuario" name='id_usuario' onChange={(e) => setResena({ ...resena, id_usuario: e.target.value })} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                         <option value="" selected>Selecciona un usuario</option>
@@ -83,7 +83,7 @@ const CrearResena = () => {
 
                 </div>
                 <div className="mb-6">
-                    <label htmlFor="id_pelicula" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pelicula</label>
+                    <label htmlFor="id_pelicula" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Pelicula</label>
                     <select id="id_pelicula" name='id_pelicula' onChange={(e) => setResena({ ...resena, id_pelicula: e.target.value })} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                     <option value="" selected>Selecciona una pelicula</option>
                         {peliculas.map(pelicula => {
@@ -94,12 +94,12 @@ const CrearResena = () => {
                     </select>
                 </div>
                 <div className="mb-6">
-                    <label htmlFor="titulo" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titulo</label>
+                    <label htmlFor="titulo" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Titulo</label>
                     <input type="text" id="titulo" name="titulo" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Titulo reseña" required
                         onChange={(e) => setResena({ ...resena, titulo: e.target.value })} ></input>
                 </div>
                 <div className="mb-6">
-                    <label htmlFor="puntuacion" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Puntuacion</label>
+                    <label htmlFor="puntuacion" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Puntuacion</label>
                     <select data-te-select-init onChange={(e) => setResena({ ...resena, puntuacion: e.target.value })} id="puntuacion" name='puntuacion'>
                         <option value="1">Uno</option>
                         <option value="2">Dos</option>
@@ -111,7 +111,7 @@ const CrearResena = () => {
                     <EstrellasRating numero={resena.puntuacion}></EstrellasRating>
                 </div>
                 <div className="mb-6">
-                    <label htmlFor="comentario" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Comentario</label>
+                    <label htmlFor="comentario" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Comentario</label>
                     <textarea id="comentario" name="comentario" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Deja un comentario..."
                         onChange={(e) => setResena({ ...resena, comentario: e.target.value })} required></textarea>
                 </div>
